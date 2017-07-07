19930aab6778847868e1fa646a4da5c21e0ed6ec
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-07-07 10:47:23 -0300

    2 players success

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,54 +1,65 @@
-// var qntPlayers = prompt("Quantos players ? 1 ou 2 ?");
-// console.log("qntPlayers: "+qntPlayers);
+var qntPlayers = 0;
 var contPlayer = 0; //Contador de vez do player
 var contMoves = 0; //Contador de jogadas
 var player = 0;
 
-//Função para pegar o 'src' da imagem
-function checkImg(id){
+//Função para verificar a quantidade de players
+function quantifyPlayers(){
+    do{
+        qntPlayers = prompt("Informe a quantidade de Player:\n1 ou 2 players?");
+    }while((qntPlayers != 1) && (qntPlayers != 2));
+}
+
+//Seleciona se quer ser X ou O
+function whoStart(){
+    contPlayer = prompt("Player 1 Escolha:\n1 - X\n2 - O");
+    
+    while((contPlayer != 1) && (contPlayer != 2)){
+        alert("Opção Inválida!");
+        whoStart();
+    }
+}
+
+//Função que verifica quem é o proximo a jogar
+function whoNext(){
+    var proximo;
+    if(contPlayer % 2 != 0){
+            player = "x";
+            proximo = "player O"
+            contPlayer++;
+            contMoves++;
+        }
+        else if(contPlayer % 2 == 0){
+            player = "o";
+            proximo = "player X"
+            contPlayer++;
+            contMoves++;
+        }
+        console.log("Vez do: "+proximo);
+}
+
+//Função para pegar o 'src' da imagem que ele clicar
+function getSrcImg(id){
     var img = document.getElementById(id).src
     return img.substring(img.length - 10, img.length);
 }
 
-//Seleciona se quer ser X ou O
-function whoStart(contPlyr, plyr){
-    this.plyr = prompt("Escolha:\n1 - X\n2 - O");
-    if(this.plyr == 1){
-        this.contPlyr = 1; //player = "x"
-    }
-    else if(this.plyr == 2){
-        this.contPlyr = 2; //player = "o"
-    }
-    else{
-        do{
-            alert("Opção Inválida!");
-            whoStart(contPlyr, plyr);
-        }while(this.plyr != 1 && this.plyr != 2);
-    }
-}
-
-whoStart(contPlayer, player);
-
 //Função chamada quando o usuário clica na tela
-function inGame(id, contPlyr, contMov){
-    var img = checkImg(id);
+function inGame(id){
+    var img = getSrcImg(id);
     if(img === "branco.png"){        
-        if(this.contPlyr % 2 != 0){
-            player = "x";
-            this.contPlyr++;
-            this.contMov++;
-        }
-        else if(this.contPlyr % 2 == 0){
-            player = "o";
-            this.contPlyr++;
-            this.contMov++;
-        }
+        whoNext();
         document.getElementById(id).src = "public/images/" +player +".png";
         document.getElementById(id).setAttribute("val", player);
+        document.getElementById(id).removeAttribute("onclick");
     }
-    // console.log("quem jogou: "+player);
-    // console.log("contPlayer: "+this.contPlayer);
-    // console.log("contMoves: "+this.contMoves);
+    //console.log("quem jogou: "+player);
+    console.log("contPlayer: "+this.contPlayer);
+    console.log("contMoves: "+contMoves);
 
-    chkResult(this.contMoves, vetImg);
+    //Verifica o resultado
+    chkResult();
 }
+
+quantifyPlayers();
+whoStart();
--- public/javascripts/result.js
+++ public/javascripts/result.js
@@ -1,110 +1,123 @@
 var vetImg = [];
+var drawX = false;
+var drawO = false;
 
 //Função para pegar o 'src' de todas as imagens do tabuleiro
-function checkAllImg(){
+function getSrcAllImg(){
     for(var i = 0; i < 9; i++){
         vetImg[i] = document.getElementById("home"+i).getAttribute("val");  
     }
-    return vetImg;
 }
 
 //Verificando se o player X é o vencedor
-function chkIfWinnerX(vetImg){
+function chkIfWinnerX(vet){
     //Linha 1
-    if((this.vetImg[0] == "x") && (this.vetImg[1] == "x") && (this.vetImg[2] == "x")){
+    if((vet[0] == "x") && (vet[1] == "x") && (vet[2] == "x")){
         winnerX();
     }
     //Linha 2
-    else if((this.vetImg[3] == "x") && (this.vetImg[4] =="x") && (this.vetImg[5] == "x")){
+    else if((vet[3] == "x") && (vet[4] =="x") && (vet[5] == "x")){
         winnerX();
     }
     //Linha 3
-    else if((this.vetImg[6] == "x") && (this.vetImg[7] == "x") && (this.vetImg[8] == "x")){
+    else if((vet[6] == "x") && (vet[7] == "x") && (vet[8] == "x")){
         winnerX();
     }
     //Diagonal primária
-    else if((this.vetImg[0] == "x") && (this.vetImg[4] == "x") && (this.vetImg[8] == "x")){
+    else if((vet[0] == "x") && (vet[4] == "x") && (vet[8] == "x")){
         winnerX();
     }
     //Diagonal secundária
-    else if((this.vetImg[2] == "x") && (this.vetImg[4] == "x") && (this.vetImg[6] == "x")){
+    else if((vet[2] == "x") && (vet[4] == "x") && (vet[6] == "x")){
         winnerX();
     }
     //Coluna 1
-    else if((this.vetImg[0] == "x") && (this.vetImg[3] == "x") && (this.vetImg[6] == "x")){
+    else if((vet[0] == "x") && (vet[3] == "x") && (vet[6] == "x")){
         winnerX();
     }
     //Coluna 2
-    else if((this.vetImg[1] == "x") && (this.vetImg[4] == "x") && (this.vetImg[7] == "x")){
+    else if((vet[1] == "x") && (vet[4] == "x") && (vet[7] == "x")){
         winnerX();
     }
     //Coluna 3
-    else if((this.vetImg[2] == "x") && (this.vetImg[5] == "x") && (this.vetImg[8] == "x")){
+    else if((vet[2] == "x") && (vet[5] == "x") && (vet[8] == "x")){
         winnerX();
     }
     else{
-        draw();
+        return drawX = true;
     }
 }
 
 //Verificando se o player O é o vencedor
-function chkIfWinnerO(vetImg){
+function chkIfWinnerO(vet){
     //Linha 1
-    if((this.vetImg[0] == "o") && (this.vetImg[1] == "o") && (this.vetImg[2] == "o")){
+    if((vet[0] == "o") && (vet[1] == "o") && (vet[2] == "o")){
         winnerO();
     }
     //Linha 2
-    else if((this.vetImg[3] == "o") && (this.vetImg[4] =="o") && (this.vetImg[5] == "o")){
+    else if((vet[3] == "o") && (vet[4] =="o") && (vet[5] == "o")){
         winnerO();
     }
     //Linha 3
-    else if((this.vetImg[6] == "o") && (this.vetImg[7] == "o") && (this.vetImg[8] == "o")){
+    else if((vet[6] == "o") && (vet[7] == "o") && (vet[8] == "o")){
         winnerO();
     }
     //Diagonal primária
-    else if((this.vetImg[0] == "o") && (this.vetImg[4] == "o") && (this.vetImg[8] == "o")){
+    else if((vet[0] == "o") && (vet[4] == "o") && (vet[8] == "o")){
         winnerO();
     }
     //Diagonal secundária
-    else if((this.vetImg[2] == "o") && (this.vetImg[4] == "o") && (this.vetImg[6] == "o")){
+    else if((vet[2] == "o") && (vet[4] == "o") && (vet[6] == "o")){
         winnerO();
     }
     //Coluna 1
-    else if((this.vetImg[0] == "o") && (this.vetImg[3] == "o") && (this.vetImg[6] == "o")){
+    else if((vet[0] == "o") && (vet[3] == "o") && (vet[6] == "o")){
         winnerO();
     }
     //Coluna 2
-    else if((this.vetImg[1] == "o") && (this.vetImg[4] == "o") && (this.vetImg[7] == "o")){
+    else if((vet[1] == "o") && (vet[4] == "o") && (vet[7] == "o")){
         winnerO();
     }
     //Coluna 3
-    else if((this.vetImg[2] == "o") && (this.vetImg[5] == "o") && (this.vetImg[8] == "o")){
+    else if((vet[2] == "o") && (vet[5] == "o") && (vet[8] == "o")){
         winnerO();
     }
     else{
-        draw();
+        return drawO = true;
     }
 }
 
+//Função para remover onclick de todas as img
+function rmOnclick(){
+    for(var i = 0; i < 9; i++){
+        document.getElementsByClassName("home")[i].removeAttribute("onclick");
+    }
+}
+
+//Função para exibir se o ganhador for o "X"
 function winnerX(){
     console.log("Winner: Player X");
+    rmOnclick()
     return;
 }
 
+//Função para exibir se o ganhador for o "O"
 function winnerO(){
     console.log("Winner: Player O");
+    rmOnclick()
     return;
 }
 
+//Função para exibir se deu Empate
 function draw(){
     console.log("Draw");
     return;
 }
 
 //Função para verificar o resultado
-function chkResult(contMoves, vetImg){
-    checkAllImg();
+function chkResult(){
+    getSrcAllImg();
     // console.log("vetImg: "+vetImg);
-    chkIfWinnerO(this.vetImg);
-    chkIfWinnerX(this.vetImg);
+    chkIfWinnerO(vetImg);
+    chkIfWinnerX(vetImg);
 }
