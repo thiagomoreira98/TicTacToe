2d3a5b9093fbc7c6a2398ca6d20ae47774081d4f
Thiago Moreira <moreira.g.thiago@gmail.com>
2017-07-13 08:08:07 -0300

    heroku part 5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let load = require("express-load");
-let port = process.env.PORT || 3000
+//let port = process.env.PORT || 3000
 
 app = express();
 
@@ -8,6 +8,6 @@ app.use(express.static(__dirname + '/'));
 
 load("controller").then("route").into(app);
 
-app.listen(port, ()=>{
+app.listen(3000, ()=>{
     console.log("SERVIDOR INICIADO COM SUCESSO!");
 });
